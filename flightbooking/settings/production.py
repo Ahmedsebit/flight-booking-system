9bd560f825c6640b993792d40cdf60bc11793b59
Ahmedsebit <Ahmed.yusuf@andela.com>
2018-10-16 16:46:02 +0300

    DEPLOY

--- flightbooking/settings/production.py
+++ flightbooking/settings/production.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.whiteNoiseMiddleware',,
+    'whitenoise.middleware.whiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'flightbooking.urls'
