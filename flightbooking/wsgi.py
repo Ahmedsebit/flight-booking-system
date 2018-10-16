d1b64443336b4dad17f616637dffcf516b427283
Ahmedsebit <Ahmed.yusuf@andela.com>
2018-10-16 16:49:59 +0300

    DEPLOY

--- flightbooking/wsgi.py
+++ flightbooking/wsgi.py
@@ -11,7 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flightbooking.settings.production')
+
+# application = DjangoWhiteNoise(application)
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
