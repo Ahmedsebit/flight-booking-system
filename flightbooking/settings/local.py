b70c8347367bc3b2568fa81af37d5dbfdd836f64
Ahmedsebit <Ahmed.yusuf@andela.com>
2018-10-17 10:51:42 +0300

    Deploy

--- flightbooking/settings/local.py
+++ flightbooking/settings/local.py
@@ -141,15 +141,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static-storage"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static-storage"),
+# ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static-serve")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static-serve")
 
 MEDIA_URL  = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 REST_FRAMEWORK = {
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
