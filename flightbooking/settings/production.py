6f26db0a2a98c1603adf14b32b09570daaded6dd
Ahmedsebit <Ahmed.yusuf@andela.com>
2018-10-17 10:53:12 +0300

    Deploy

--- flightbooking/settings/production.py
+++ flightbooking/settings/production.py
@@ -169,18 +169,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static-storage'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static-storage'),
+# )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static-serve")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static-serve")
 
 MEDIA_URL  = '/media/'
 
