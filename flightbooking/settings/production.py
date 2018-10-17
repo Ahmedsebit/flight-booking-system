86264372119ced32bc965d3077d47e27a9fe5511
Ahmedsebit <Ahmed.yusuf@andela.com>
2018-10-17 10:50:04 +0300

    Deploy

--- flightbooking/settings/production.py
+++ flightbooking/settings/production.py
@@ -177,8 +177,10 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'static-storage'),
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static-serve")
 
 MEDIA_URL  = '/media/'
 
