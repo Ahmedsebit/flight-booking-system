ecd1e715244bb1587d9531c2355aa0e084005110
Ahmedsebit <Ahmed.yusuf@andela.com>
2018-10-17 14:20:28 +0300

    Deploy

--- flight/api/views.py
+++ flight/api/views.py
@@ -36,7 +36,7 @@ class FlightApiListView(generics.ListAPIView):
 class FlightAPICreateView(generics.CreateAPIView):
 
     serializer_class = FlightModelSerializer
-    permission_classes = [permissions.IsStaff]
+    permission_classes = [permissions.IsAdminUser]
 
 
 class FlightApiDetailView(generics.RetrieveAPIView):
@@ -49,11 +49,11 @@ class FlightApiDestroyView(generics.DestroyAPIView):
 
     queryset = Flight.objects.all()
     serializer_class = FlightModelSerializer
-    permission_classes = [permissions.IsStaff]
+    permission_classes = [permissions.IsAdminUser]
 
 
 class FlightApiUpdateView(generics.UpdateAPIView):
 
     queryset = Flight.objects.all()
     serializer_class = FlightModelUpdateSerializer
-    permission_classes = [permissions.IsStaff]
+    permission_classes = [permissions.IsAdminUser]
