54662f6fa97f096b722d843d101530d10becdbae
Ahmedsebit <Ahmed.yusuf@andela.com>
2018-10-22 10:33:25 +0300

    REFACTOR

--- seats/api/urls.py
+++ seats/api/urls.py
@@ -3,7 +3,7 @@ from .views import SeatApiListView, SeatApiDetailView, SeatApiDestroyView, SeatA
 
 urlpatterns = [
     url(r'^$', SeatApiListView.as_view(), name='seat_list'),
-    url(r'^create$', SeatApiCreateView.as_view(), name='create'),
+    url(r'^create/$', SeatApiCreateView.as_view(), name='create'),
     url(r'^(?P<pk>\d+)/delete/$', SeatApiDestroyView.as_view(), name='delete'),
     url(r'^(?P<pk>\d+)/detail/$', SeatApiDetailView.as_view(), name='detail'),
     url(r'^(?P<pk>\d+)/update/$', SeatApiUpdateView.as_view(), name='update'),
